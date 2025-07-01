ee2095ee23599061465d384972770be101e8aff4
Manaf <manaf.kassim-22@cpu.edu.ph>
2025-07-01 19:16:09 +0800

    added fallbackTimer on index (ai rerview suggestion)

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,18 @@
+import { useRouter } from "expo-router";
+import { useEffect } from "react";
 import { ActivityIndicator, View } from "react-native";
 
 export default function Index() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const fallbackTimer = setTimeout(() => {
+      router.replace("/(auth)/login");
+    }, 3000);
+
+    return () => clearTimeout(fallbackTimer);
+  }, [router]);
+
   return (
     <View className="flex-1 justify-center items-center bg-white">
       <ActivityIndicator size="large" color="#FF7200FF" />
