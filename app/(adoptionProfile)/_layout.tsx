3aaff65ed413091ce03f78cc78718bf748b00961
Jokkeeerrz <143706437+Jokkeeerrz@users.noreply.github.com>
2025-07-02 21:28:56 +0800

    update adoptionProfile

--- app/(adoptionProfile)/_layout.tsx
+++ app/(adoptionProfile)/_layout.tsx
@@ -1,9 +1,9 @@
 import { Stack } from "expo-router"
 
-export default function RootLayout() {
+export default function layout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="page" options={{ headerShown: false }} />
     </Stack>
   )
 }
