8f89b48e11651117947ad99a203e70f0dd8483d3
Jokkeeerrz <143706437+Jokkeeerrz@users.noreply.github.com>
2025-07-06 20:38:17 +0800

    edit

--- app/(userTabs)/home.tsx
+++ app/(userTabs)/home.tsx
@@ -406,7 +406,7 @@ const VideoItemComponent = ({
             >
               <Text className="text-lg font-bold text-black">A</Text>
             </View>
-            <Text className="mt-1 text-xs text-white">Adapt</Text>
+            <Text className="mt-1 text-xs text-white">Adopt</Text>
           </Pressable>
 
           {/* Share Button */}
